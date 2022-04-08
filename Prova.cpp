58b04a39f06a7642ff3b63307b68415c8aa4c7f5
AleSciacca <ale.sciacca25@gmail.com>
2022-04-09 01:14:50 +0200

    Update Prova.cpp

--- Prova.cpp
+++ Prova.cpp
@@ -5,14 +5,19 @@
 using namespace std;
 
 int main(){
-    ifstream porta("Portieri.txt");
+    int DIM=0;
+    string player;
+    string** portieri;
+    int i,j;
+
+    ifstream porta("portieri.txt");
 
     if(!porta){
         cout<<"Problema apertura file...";
     }else{
         string nome="";
 
-       /* string squadra="";*/
+       string squadra="";
 
         string quotazione_attuale_str="";
         int quotazione_attuale=0;
@@ -20,20 +25,33 @@ int main(){
         string quotazione_iniziale_str="";
         int quotazione_iniziale=0;
 
-        while(getline(porta,nome,';')){  //chiediamo al tool di andare avanti fino a quando non trova un carattere ";" ,tutto ciò che legge prima verra messo in cognome
-           /* getline(porta,squadra,';');*/
+        while(getline(porta,nome,';')){ 
+            i=0; //chiediamo al tool di andare avanti fino a quando non trova un carattere ";" ,tutto ciò che legge prima verra messo in cognome
+            getline(porta,squadra,';');
             getline(porta,quotazione_attuale_str,';');
             getline(porta,quotazione_iniziale_str);
-          
 
-            cout<<"Giocatore: "<<nome<<endl;
-           /* cout<<"Squadra: "<<squadra<<endl;*/
-            cout<<"Quotazione attuale: "<<quotazione_attuale<<endl;
-            cout<<"Quotazione iniziale: "<<quotazione_iniziale<<endl;
+            portieri[i][j] = nome;
+            portieri[i+1][j] = squadra;
+            portieri[i+2][j] = quotazione_attuale_str;
+            portieri[i+3][j] = quotazione_iniziale_str;
 
-            cout<<string(40,'=')<<endl;
+            DIM++;
+            j++;            
         }  
         porta.close(); porta.clear();
+
+        cout<<DIM<<endl;
+
+        int righe=DIM;
+        int colonne=4;
+
+        cout<<"INSERIRE IL NOME DEL GIOCATORE CHE STAI CERCANDO: ";
+        cin>>player;
+
+       /* for(int i=0;i<righe;i++){
+            if(player)
+        }*/
 
     }
     return 0;
