d0a72c21eed8088db769bc6094b59933129fd6c4
AleSciacca <ale.sciacca25@gmail.com>
2022-05-20 18:19:00 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "calciatore.h"
+#include <iostream>
+#include <fstream>
+
+#define MAX_N 400
+
+using namespace std;
+
+int main(){
+
+    int n=MAX_N;
+
+    Calciatore* arr= new Calciatore[n];
+}
