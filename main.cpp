ebd2de8a34daca73ecfbf81c9b162e97cdc9815d
AleSciacca <ale.sciacca25@gmail.com>
2022-05-21 10:08:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <fstream>
 
-#define MAX_N 400
+#define MAX_N 245
 
 using namespace std;
 
@@ -14,11 +14,14 @@ int main(){
     ifstream ifs("listone.txt");
     Calciatore* arr= new Calciatore[n];
     unsigned int count=0;
-    while(ifs.good() && count<n){
+    while(count<n){
         Calciatore cal;
         ifs>>cal;
-        //arr[count++] = cal;
+        arr[count++] = cal;
         cout<<cal<<endl;
+
+        // non so il motivo ma se provo ad aprire il file exe non riesco a leggere nulla e crasha
+        
         
     }
 
